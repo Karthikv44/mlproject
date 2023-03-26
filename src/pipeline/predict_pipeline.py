723f423df7daf3abed188bc13d4af5dde4aeedde
Karthikv44 <karthikv2020ai@gmail.com>
2023-03-26 18:32:33 +0530

    Application

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -11,7 +11,7 @@ class PredictPipeline:
     def predict(self,features):
         try:
             model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            preprocessor_path='artifacts\proprocessor.pkl'
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
